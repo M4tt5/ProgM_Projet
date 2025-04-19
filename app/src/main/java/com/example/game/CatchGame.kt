77833f43d384ac44b5fb6b718faa6a1486a01d0a
mdaviaud <matthis.daviaud@etudiant.univ-rennes1.fr>
2025-04-19 21:20:40 +0200

    Ajout du nouveau jeu ShotGame et refonte des boutons retour sur les menus; mise en place des classes pour la partie rapide à 1 joueur

--- app/src/main/java/com/example/game/CatchGame.kt
+++ app/src/main/java/com/example/game/CatchGame.kt
@@ -1,13 +1,46 @@
 package com.example.game
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 
-class CatchGame : ComponentActivity() {
+class CatchGame : ComponentActivity(), CatchGameView.OnCatchGameFinishedListener {
+
+    private var isQuickPlay = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        isQuickPlay = intent.getBooleanExtra("quickPlay", false)
+        showIntroScreen()
+    }
 
-        // Utilisation directe de la vue personnalisée comme contenu de l'activité
-        setContentView(CatchGameView(this))
+    private fun showIntroScreen() {
+        setContentView(R.layout.catchgame)
+
+        val startButton = findViewById<Button>(R.id.btnStartGame)
+        val retourButton = findViewById<Button>(R.id.btnRetour)
+
+        startButton.setOnClickListener {
+            startGame()
+        }
+
+        retourButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun startGame() {
+        if (isQuickPlay) {
+            setContentView(CatchGameView(this, null, true, this))
+        } else {
+            setContentView(CatchGameView(this))
+        }
+    }
+
+    override fun onGameFinished(score: Int) {
+        val resultIntent = intent
+        resultIntent.putExtra("score", score)
+        setResult(RESULT_OK, resultIntent)
+        finish()
     }
 }
